5da0db2086ebd3ddc704b1b6ed8230524d7aa97a
Assem Yasser <assem.yasser200@gmail.com>
2022-01-27 01:51:39 +0200

    - Language Select - Pause MEnu Fixes

---
+++ Assets/_Game/Scripts/UI/LanguageSelect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using I2.Loc;
+
+public class LanguageSelect : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI arabicBtnTextUI;
+    private string arabicBtnText;
+
+    private void Start()
+    {
+        // arabicBtnTextUI.text = I2.Loc.RTLFixer.Fix(arabicBtnTextUI.text);
+    }
+
+    public void RTLFix(TextMeshProUGUI textUI)
+    {
+        if (string.IsNullOrEmpty(arabicBtnText))
+            arabicBtnText = textUI.text;
+
+        Debug.Log(arabicBtnText);
+
+        // textUI.isRightToLeftText = true;
+        textUI.text = I2.Loc.RTLFixer.Fix(arabicBtnText);
+
+        Debug.Log(textUI.text);
+    }
+}
