1a0a5b72251d35b77b39b56af3a0ebb18a8391b2
Assem Yasser <assem.yasser200@gmail.com>
2022-01-26 11:41:52 +0200

    trigger shop by player

--- Assets/Scripts/Managers/ShopManager.cs
+++ Assets/Scripts/Managers/ShopManager.cs
@@ -1,15 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.U2D.Animation;
-using System.Linq;
 
 public class ShopManager : MonoBehaviour
 {
-
-    [SerializeField] private SpriteLibraryAsset libraryAsset;
     [SerializeField] private ShopItemData[] shopItemsData;
     [SerializeField] private UiShopItem shopItem;
+    [SerializeField] private Transform shopCanvas;
     [SerializeField] private Transform shopItemsPanel;
     private IShopCustomer shopCustomer;
     private string[] labels;
@@ -17,13 +14,6 @@ public class ShopManager : MonoBehaviour
 
     void Start()
     {
-        // categoryNames = libraryAsset.GetCategoryNames().ToArray(); 
-
-        // for(int i = 0; i < categoryNames.Length; i++)
-        // {
-        //     labels = libraryAsset.GetCategoryLabelNames(categoryNames[i]).ToArray();
-        // }
-
         for(int i = 0; i < shopItemsData.Length; i++)
         {
             UiShopItem shopItemInstance = Instantiate(shopItem, shopItemsPanel, false);
@@ -36,8 +26,14 @@ public class ShopManager : MonoBehaviour
 
     void CheckItemPrice(int price, string category, string label)
     {
-        Debug.Log(price +" , "+ category);
-        Debug.Log(shopCustomer);
+        //Debug.Log(price +" , "+ category +" , "+ label);
+
         shopCustomer.BoughtItem(label, category);
+    }
+
+    public void StartShopInteraction(IShopCustomer player)
+    {
+        this.shopCustomer = player;
+        shopCanvas.gameObject.SetActive(true);
     }
 }
